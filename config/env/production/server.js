30f3706fee64fd6eac34832e708a106fc8f3be2b
mtgezgin <mehmetturgutgezgin@gmail.com>
2022-09-07 16:12:06 +0300

    Update server config.

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,15 @@
 module.exports = ({ env }) => ({
-    url: env('MY_HEROKU_URL'),
-  });
+  url: env("MY_HEROKU_URL"),
+  proxy: true,
+  app: {
+    keys: env.array("APP_KEYS", [
+      "DATABASE_URL",
+      "HEROKU_POSTGRESQL_MAROON_URL",
+    ]),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
+});
